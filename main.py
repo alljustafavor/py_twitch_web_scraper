cd55372dada7b18bc113d23cdac177ec02fe6215
Joseph Noblett <noblettjoe@gmail.com>
2024-02-26 20:17:46 -0500

    refactored get_chat_messages function

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 
-service = Service(executable_path="/usr/bin/chromedriver")
-driver = webdriver.Chrome(service=service)
-driver.maximize_window()
-driver.get("https://twitch.tv/lord_kebun")
-
-time.sleep(10)
-
-wait = WebDriverWait(driver, 10) # Timeout after 10 seconds
-chat_container = driver.find_element(By.CLASS_NAME, "simplebar-content")
 chat_log = []
 
-while True:
-    chatter = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "chat-author__display-name")))
-    chat_message = wait.until(EC.presence_of_element_located((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/aside[1]/div[1]/div[1]/div[2]/div[1]/div[1]/section[1]/div[1]/div[4]/div[2]/div[2]/div[3]/div[1]/div[1]/div[last()]//div[1]/div[2]/div[1]/div[1]/span[2]/span[1]")))
-    chat_log.append(chat_message.text)
-    print(chat_log)
-    time.sleep(4)
+def get_chat_messages():
+    service = Service(executable_path="/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    driver.get("https://twitch.tv/lord_kebun")
 
-driver.quit()
+    wait = WebDriverWait(driver, 10)
+    chat_container = driver.find_element(By.CLASS_NAME, "simplebar-content")
+    
+    while len(chat_log) < 8:
+        chatter = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "chat-author__display-name")))
+        chat_message = wait.until(EC.presence_of_element_located((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/aside[1]/div[1]/div[1]/div[2]/div[1]/div[1]/section[1]/div[1]/div[4]/div[2]/div[2]/div[3]/div[1]/div[1]/div[last()]//div[1]/div[2]/div[1]/div[1]/span[2]/span[1]")))
+        chat_log.append(chat_message.text)
+        print(chat_log)
+        time.sleep(2)
+
+    driver.quit()
+
+get_chat_messages()
+#def get_message_score(message_arr):
+    
