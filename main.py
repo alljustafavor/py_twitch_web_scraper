d94898e12c33cc6dba13e34e9544364adf3a3f9a
Joseph Noblett <noblettjoe@gmail.com>
2024-02-27 01:45:10 -0500

    added sentiment analis function, can get avg sentiment but need to add my own custom lexicon for twitch emotes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#senlenium imports
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -5,15 +7,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+#nltk  imports
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+#default imports
 import time
 
 chat_log = []
+sentiment_score_array = []
+sentiment_avg = 0
 
 def get_chat_messages():
     service = Service(executable_path="/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=service)
     driver.maximize_window()
-    driver.get("https://twitch.tv/lord_kebun")
+    driver.get(f"https://twitch.tv/summit1g")
 
     wait = WebDriverWait(driver, 10)
     chat_container = driver.find_element(By.CLASS_NAME, "simplebar-content")
@@ -28,5 +38,29 @@ def get_chat_messages():
     driver.quit()
 
 get_chat_messages()
-#def get_message_score(message_arr):
-    
+
+def get_message_score(message_arr):
+    nltk.download([
+        "vader_lexicon",
+    ])
+    sia = SentimentIntensityAnalyzer()
+    for message in message_arr:
+        scores = sia.polarity_scores(message)
+        sentiment_score_array.append(scores)
+        print(sentiment_score_array)
+    return sentiment_score_array
+
+get_message_score(chat_log)
+
+def get_average_sentiment(score_array):
+    sentiment_score = 0.0
+    for item in score_array:  
+        sentiment_score += item['compound']  
+    sentiment_avg = sentiment_score / 8
+    print(sentiment_avg)
+    return sentiment_avg 
+
+get_average_sentiment(sentiment_score_array)
+
+def most_common_chat():
+    pass
